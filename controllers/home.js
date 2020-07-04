717ed9383a502d386607d31ec630f34552ee67cd
wayneherridge <wsherridge@icloud.com>
2020-07-04 18:48:38 +0100

    Updated file

--- controllers/home.js
+++ controllers/home.js
@@ -1,8 +1,17 @@
 const BlogPost = require('../models/BlogPost.js')
 
-module.exports = async (req, res) =>{
-    const blogposts = await BlogPost.find({})         
-    res.render('index',{
+module.exports = async (req, res) => {
+    const blogposts = await BlogPost.find({})
+        .then(data => {
+            return data.json()
+        })
+        .catch(err => {
+            console.log(err)
+            res.json({
+                message: err.message
+            })
+        })
+    res.render('index', {
         blogposts
     });
 }
