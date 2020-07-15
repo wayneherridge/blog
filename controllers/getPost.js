157a076d86c6ba16945670599aa548671f9c4bcb
wayneherridge <wsherridge@icloud.com>
2020-07-15 21:04:27 +0100

    Updated file

--- controllers/getPost.js
+++ controllers/getPost.js
@@ -1,9 +1,9 @@
 const BlogPost = require('../models/BlogPost.js')
 
-module.exports = async (req,res)=>{        
-    const blogpost = await BlogPost.findById(req.params.id)
+module.exports = async (req, res) => {
+    const blogpost = await (await BlogPost.findById(req.params.id)).populated('userid')
     console.log(blogpost)
-    res.render('post',{
+    res.render('post', {
         blogpost
-    });    
+    });
 }
