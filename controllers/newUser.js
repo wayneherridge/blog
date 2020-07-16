0dc109cf0fde08c252edf18cb7dabf95d033a702
wayneherridge <wsherridge@icloud.com>
2020-07-16 18:48:06 +0100

    Updated file

--- controllers/newUser.js
+++ controllers/newUser.js
@@ -1,17 +1,15 @@
 module.exports = (req, res) => {
-
     var username = ""
     var password = ""
-    const data = req.flash('data')[0]
+    const data = req.flash('data')[0];
 
     if (typeof data != "undefined") {
         username = data.username
         password = data.password
     }
 
-
     res.render('register', {
-        errors: flash('validationErrors'),
+        errors: req.flash('validationErrors'),
         username: username,
         password: password
     })
